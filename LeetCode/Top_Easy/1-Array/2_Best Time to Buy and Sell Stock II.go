5a84a3c15d9e4c58985e23042836ad780cf9d753
abrartariq <abrartariq14@gmail.com>
2022-05-21 21:11:51 -0500

    Day 1

--- LeetCode/Top_Easy/1-Array/2_Best Time to Buy and Sell Stock II.go
+++ LeetCode/Top_Easy/1-Array/2_Best Time to Buy and Sell Stock II.go
@@ -5,10 +5,17 @@ import "fmt"
 
 func main(){
 
-	Input := []int{7,1,5,3,6,4}
-	fmt.Println(removeDuplicates(tempinput))
+	Input := []int{7,1,5,3,6,7,8,9}
+	fmt.Println(maxProfit(Input))
 }
 
 func maxProfit(prices []int) int {
+    var profit int
+    for i := 0; i < len(prices)-1; i++ {
+        if prices[i] < prices[i+1] {
+            profit += prices[i+1] - prices[i]
+        }
+    }
     
+    return profit
 }

dac5e8dd369c52c4e77b5592caa35ec3abaf1a82
abrartariq <abrartariq14@gmail.com>
2022-05-26 22:17:05 -0500

    7

---
+++ LeetCode/Top_Easy/2-Strings/02_Reverse Integer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main(){
+	// Input := []byte("ABC")
+	// reverseString(Input)
+	fmt.Println(reverse(92313))
+}
+
+func reverse(x int) int {
+    
+    result := 0;
+    for x != 0 {
+        result = result * 10 + x % 10;
+        x = x / 10;
+    }
+    
+    if result > math.MaxInt32 || result < math.MinInt32{
+        return 0
+    }
+
+    return result
+}
+
+

33cb8a915fb06841864daa12ba3e55439548435c
abrartariq <abrartariq14@gmail.com>
2022-09-21 20:47:33 -0500

    Day 33

---
+++ LeetCode/Top_Easy/3-Linked List/06_Linked list Cycle.go
@@ -0,0 +1,28 @@
+package main
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+ 
+
+func main(){
+
+}
+
+func hasCycle(head *ListNode) bool {
+    if head == nil{
+        return false
+    }
+    nHead := head.Next
+    
+    for head != nil && nHead != nil && nHead.Next != nil {
+        if head == nHead{
+            return true
+        }
+        head = head.Next
+        nHead = nHead.Next
+        nHead = nHead.Next
+    }
+    return false
+}

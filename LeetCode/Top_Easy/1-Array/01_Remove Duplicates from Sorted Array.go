4b3386d2244a1ca4086016e57a7344d252945060
Tariq <mtariq5@uic.edu>
2023-05-12 00:25:37 -0500

    Tree Done

--- LeetCode/Top_Easy/1-Array/01_Remove Duplicates from Sorted Array.go
+++ LeetCode/Top_Easy/1-Array/01_Remove Duplicates from Sorted Array.go
@@ -2,22 +2,21 @@ package main
 
 import "fmt"
 
+func main() {
 
-func main(){
-
-	Input := []int{1,2,3,4}
-	fmt.Println(removeDuplicates(tempinput))
+	tempInput := []int{1, 2, 3, 4}
+	fmt.Println(removeDuplicates(tempInput))
 }
 
 func removeDuplicates(nums []int) int {
-    a := 0
-    
-    for b := 1; b < len(nums); b++ {
-        if nums[a] != nums[b] {
-            a++
-            nums[a] = nums[b]
-        }
-    }
-    
-    return a + 1
+	a := 0
+
+	for b := 1; b < len(nums); b++ {
+		if nums[a] != nums[b] {
+			a++
+			nums[a] = nums[b]
+		}
+	}
+
+	return a + 1
 }

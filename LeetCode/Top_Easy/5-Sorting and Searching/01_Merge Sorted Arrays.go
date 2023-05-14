604588537aed294adfb2999b4b438a10ea998ff1
Tariq <mtariq5@uic.edu>
2023-05-13 23:49:04 -0500

    Daily

---
+++ LeetCode/Top_Easy/5-Sorting and Searching/01_Merge Sorted Arrays.go
@@ -0,0 +1,30 @@
+import "fmt"
+
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+
+	if m < n{
+		m, n = n, m
+		nums1, nums2 = nums2, nums1
+	}
+
+
+
+
+
+}
+
+
+func main(){
+
+}
+
+

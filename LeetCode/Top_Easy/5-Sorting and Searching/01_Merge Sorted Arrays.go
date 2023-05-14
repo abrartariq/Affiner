19094b15cafde090a91e9a8790b18740eb75e6d6
abrartariq <abrartariq14@gmail.com>
2023-05-14 16:59:05 -0500

    Day 92

--- LeetCode/Top_Easy/5-Sorting and Searching/01_Merge Sorted Arrays.go
+++ LeetCode/Top_Easy/5-Sorting and Searching/01_Merge Sorted Arrays.go
@@ -1,25 +1,26 @@
 import "fmt"
 
 
-type TreeNode struct {
-	Val int
-	Left *TreeNode
-	Right *TreeNode
-}
-
-
-
 func merge(nums1 []int, m int, nums2 []int, n int)  {
+    i, j, k := m-1, n-1, m+n-1
 
-	if m < n{
-		m, n = n, m
-		nums1, nums2 = nums2, nums1
+	for i > -1 && j > -1 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			k--
+			i--
+		}else{
+			nums1[k] = nums2[j]
+			k--
+			j--
+		}
 	}
-
-
-
-
-
+    
+    if j > -1{
+        for idx,val := range nums2[:j+1]{
+			nums1[idx] = val
+		}
+    }
 }
 
 
